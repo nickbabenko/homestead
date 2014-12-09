7d0d1600a8ce4addd85c883b12068710eafc9f5c
Nick Babenko <nickbabenko@me.com>
2014-12-09 23:06:40 +0000

    Fixed access log directory

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -4,7 +4,7 @@ block="<VirtualHost *:80>
 	ServerName $1
 	DocumentRoot $2
 	ErrorLog /var/log/httpd/$1-error.log
-	CustomLog /var/log/http/$1-access.log combined
+	CustomLog /var/log/httpd/$1-access.log combined
 	<Directory "$2">
 		AllowOverride All
 		Options Indexes Includes FollowSymLinks
